7177500d83dad50e655d58f13567941266647bf4
Volodymyr Stelmakh <volodymyr.kai@gmail.com>
2024-06-30 15:54:32 +0300

    Update 1.ts

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function fetchData<T>(url: string): Promise<T> | never {
   try {
-    const response = await axios.get(url);
+    const response = await axios.get<T>(url);
     return response.data;
   } catch (error) {
     throw new Error(`Error fetching from ${url}: ${error}`);
